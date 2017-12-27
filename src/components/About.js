f0f549e2916a327366f2b38db158688a142e3752
crossy87 <hannabmcross@gmail.com>
2017-12-27 19:49:59 +0900

    css updated

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,18 @@ const About = () => {
         <h1>About</h1>
       </div>
       <div className="about-text">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis tellus id. Augue lacus viverra vitae congue eu. Feugiat in ante metus dictum at tempor commodo ullamcorper. Convallis posuere morbi leo urna molestie at elementum. Nunc id cursus metus aliquam eleifend. Adipiscing elit ut aliquam purus sit. Cum sociis natoque penatibus et magnis dis parturient. Pellentesque elit eget gravida cum sociis natoque. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Scelerisque purus semper eget duis at tellus at urna. Quam quisque id diam vel. Tincidunt vitae semper quis lectus nulla at. Orci ac auctor augue mauris augue neque gravida in. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sed velit dignissim sodales ut eu sem. Morbi leo urna molestie at.</p>
-        {/* <p>I have always been interested in visual culture and this led me to a career in Art.</p>
-          <br/>
-          <p>My passion for creative practices and communication then directed me to web development. </p>
-          <br/>
-          <p>I signed up to a couple of courses to explore the interest and fell in love with coding and so I decided to make a change and learn to be a Web Developer at GA.</p>
-          <br/>
-          <p>I am currently looking to join a creative, playful, supportive workplace where I can continue to learn and expand my knowledge and skills as well as make a positive social/environmental impact.</p> */}
+        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis tellus id. Augue lacus viverra vitae congue eu. Feugiat in ante metus dictum at tempor commodo ullamcorper. Convallis posuere morbi leo urna molestie at elementum. Nunc id cursus metus aliquam eleifend. Adipiscing elit ut aliquam purus sit. Cum sociis natoque penatibus et magnis dis parturient. Pellentesque elit eget gravida cum sociis natoque. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Scelerisque purus semper eget duis at tellus at urna. Quam quisque id diam vel. Tincidunt vitae semper quis lectus nulla at. Orci ac auctor augue mauris augue neque gravida in. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sed velit dignissim sodales ut eu sem. Morbi leo urna molestie at.</p> */}
+        <p>In 2017 I made the decision which I had been toying with for a while. I decided I wanted to become a web developer. For real.</p>
+        <p>I remember at School I enjoyed playing around with Microsoft Access to make a database for a ficticious art gallery I hoped to own one day. I also remember experimenting with HTML to make my own fan-pages with Geocities.</p>
+        <p>Fast forward to 2015 and I was feeling frustrated with elements of my job and so I did the Decoded, "Learn to code in a day" course which I loved. I had not felt so excited and passionate about a subject since I had done my undergraduate degree. However, I was not quite ready to break from my career path at that point. I wasn't sure I would be able to commit financially and mentally to a complete change in direction when I had worked so hard to make it in the job I was in.</p>
+        <p>Later that year I was told about coding bootcamps. This was the beginning of the programming epiphany. Still nervous I decided to do an online course with General Assembly to double check that web development was really and truly what I wanted to do.</p>
+        <p>It was 8 weeks of remote learning ( using a chromebook!! ) with a focus on HTML and CSS. I loved it.</p>
+        <p>By January 2017 I knew I wanted to take the leap. And so I did. I turned 30, quit my job and became a student once more. I enrolled in the General Assembly London Web Development Immersive course and from September to December 2017 I studied harder than I can remember. </p>
+        <p> Which leads me to now. And to you, dear reader.</p>
+
+        <p>I love learning to code. I want to keep doing it. I look forward to seeing where this programming adventure will lead next.</p>
+
+
       </div>
     </div>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,6 @@ const Home = () => {
       <div className="heading-home">
         <h1>HANNAH CROSS</h1>
       </div>
-      <div className="home-text">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis tellus id. Augue lacus viverra vitae congue eu. Feugiat in ante metus dictum at tempor commodo ullamcorper. Convallis posuere morbi leo urna molestie at elementum. Nunc id cursus metus aliquam eleifend. Adipiscing elit ut aliquam purus sit. Cum sociis natoque penatibus et magnis dis parturient. Pellentesque elit eget gravida cum sociis natoque. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Scelerisque purus semper eget duis at tellus at urna. Quam quisque id diam vel. Tincidunt vitae semper quis lectus nulla at. Orci ac auctor augue mauris augue neque gravida in. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sed velit dignissim sodales ut eu sem. Morbi leo urna molestie at.</p>
-      </div>
     </div>
   );
 };

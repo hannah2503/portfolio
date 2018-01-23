d70f4d98c2580ab98acc26540d5014da9c8ef259
crossy87 <hannabmcross@gmail.com>
2018-01-23 10:39:47 +0000

    ensure top of page loads on refresh

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 
 $(() => {
 
+  $(this).scrollTop(0);
+
   const $menuItems = $('.nav-item');
   const $toggle = $('.toggle');
   const $showSingle = $('.showSingle');
   const $targetDiv = $('.targetDiv');
-
-  // $menuItems.show();
 
   $toggle.click(function() {
     $menuItems.slideToggle('slow');
